a97af534f773173d1a898e26a3622523fac7e3f5
Mr1el <marouanelghazzalimr1@gmail.com>
2023-06-15 08:06:14 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,11 @@
 /**
  * main - Entry point
  *
- * description: print with puts focntion
+ * Return: Always 0 (Success)
  *
- * Return: Always 0 (success)
- */
-int main(void) 
+*/
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+     puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
